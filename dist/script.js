e7cf1eef256c433b43a19787599c849c9b77f521
Pranshu Chaudhary <wittywit@users.noreply.github.com>
2021-08-17 05:27:27 +0530

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -1,34 +1,32 @@
 window.onload = function() {
+
     //create a new instance of shake.js.
     var myShakeEvent = new Shake({
         threshold: 15
     });
+
     // start listening to device motion
     myShakeEvent.start();
+
     // register a shake event
     window.addEventListener('shake', shakeEventDidOccur, false);
-    //shake event callback
-    function shakeEventDidOccur () {
-        //put your own code here etc.
-      add();
-        //alert('Shake!');
-    }
-    $('span').bind( "touchstart", function(e){add();} );
-
-
-};
-
-
-function runme() {
+	
+	//random website array
+	function runme() {
     var arr = ["http://www.bbc.co.uk/", "http://www.yahoo.com/", "http://www.stackoverflow.com/"];
     var value = arr[Math.floor(Math.random() * arr.length)];
     window.open(value);
     // window.location = value;     // remove the comment at the beginning to actually navigate
-}
+	}
+
+	
+    //shake event callback
+    function shakeEventDidOccur () {
+
+        //put your own code here etc.
+        alert('Shake!');
+		runme();
+    }
+};
 
 
-add = function(){
-  runme();
-   // window.open("https://codepen.io/wittywit/full/wvdbooK");
- 
-}
